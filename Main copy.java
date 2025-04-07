90aecb07cf079d7d41bbc740079db733c7f8b10f
joanamonteiro25 <up202403269@up.pt>
2025-04-07 15:09:59 +0100

    alterações na classe Main copy (pessoa)
    Adicionei a classe Main para testar a classe Pessoa e cálculo de IMC
    
    Criei uma instância da classe Pessoa com nome, idade, altura e peso;
    Exibe as informações da pessoa no terminal;
    Calcula e mostra o IMC e a respetiva classificação com base nos dados fornecidos;
    Permite validar o funcionamento dos métodos calcularIMC() e classificarIMC().

--- Main copy.java
+++ Main copy.java
@@ -1,20 +1,20 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// Programa principal para testar a classe Pessoa
 public class Main {
     public static void main(String[] args) {
 
-     Pessoa pessoa = new Pessoa ();
-     pessoa.setNome ("Ju");
-     pessoa.setIdade(19);
-     pessoa.setAltura(1.55);
-     pessoa.setPeso(45);
+        // Cria uma pessoa e define seus dados
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Ju");
+        pessoa.setIdade(19);
+        pessoa.setAltura(1.55);
+        pessoa.setPeso(45);
 
+        // Exibe os dados da pessoa e o resultado do IMC
         System.out.println("Nome: " + pessoa.getNome());
         System.out.println("Idade: " + pessoa.getIdade());
         System.out.println("Altura: " + pessoa.getAltura());
         System.out.println("Peso: " + pessoa.getPeso());
         System.out.println("IMC: " + pessoa.calcularIMC());
         System.out.println("Classificação: " + pessoa.classificarIMC());
-
     }
 }
